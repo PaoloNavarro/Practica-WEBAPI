d642964c319da9b9b85a2a6388b84272df291d25
Estudiante <Estudiante@B47PC20>
2023-02-25 13:12:42 -0600

    Agregar archivos de proyecto.

---
+++ WEB-API/Controllers/EquiposController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WEB_API.Models;
+
+namespace WEB_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposConext;
+
+        public EquiposController(equiposContext equiposContext)
+        {
+            _equiposConext= equiposContext;
+        }
+        [HttpGet]
+        [Route("getall")]
+
+        public IActionResult ObtenerEquipos()
+        {
+            List<equipos> ListadoEquipo =(from e in _equiposConext.equipos select e).ToList();
+            if (ListadoEquipo.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(ListadoEquipo);
+        }
+    }
+}
---
+++ WEB-API/Models/equiposContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WEB_API.Models
+{
+    public class equiposContext : DbContext
+    {
+        public equiposContext(DbContextOptions<equiposContext> option):base(option)
+        {
+
+        }
+        public DbSet<equipos> equipos {get;set;}
+    }
+}
